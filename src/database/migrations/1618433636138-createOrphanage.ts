663784ff0b8bd8e9870e46115dcbbd5c7dd2196d
Higor Cerqueira dos Santos <higorcsantos18@gmail.com>
2021-04-14 19:33:29 -0300

    Create Table Orphanages

--- src/database/migrations/1618433636138-createOrphanage.ts
+++ src/database/migrations/1618433636138-createOrphanage.ts
@@ -1,11 +1,54 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class createOrphanage1618433636138 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'orphanages',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isGenerated: true,
+                    generationStrategy: 'uuid',
+                    isPrimary: true
+                },
+                {
+                    name: 'name',
+                    type: 'varchar'
+                },
+                {
+                    name: 'latitude',
+                    type: "decimal",
+                    precision: 2,
+                    scale: 10
+                },
+                {
+                    name: 'longitude',
+                    type: "decimal",
+                    precision: 2,
+                    scale: 10
+                },
+                {
+                    name: 'about',
+                    type: 'text'
+                },
+                {
+                    name: "instructions",
+                    type: "text"
+                },
+                {
+                    name: 'open_on_weekends',
+                    type: 'boolean',
+                    default: false
+                }
+
+            ]
+        }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('orphanages')
     }
 
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import express, { json } from 'express';
+import express from 'express';
 import routes from './routes';
+import 'reflect-metadata';
 import './database/connection'
 
 const app = express();
 const port = 3333;
-app.use(json());
+app.use(express.json());
 app.use(routes)
 
 app.listen(port, () => console.log("Server is Running"));
